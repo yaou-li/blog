92eb802a1c7906dc5a537da9f1081c3d6ab3f076
yaou-li <amor89813@gmail.com>
2016-08-03 19:42:48 -0400

    new article page

--- src/GarlicBlog/AppBundle/Controller/ArticleController.php
+++ src/GarlicBlog/AppBundle/Controller/ArticleController.php
@@ -7,10 +7,47 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class ArticleController extends Controller
 {
+    
     /**
-     * @Route("/articles/{id}", name="articles")
+     * @Route("/newArticle", name="new_article")
      */
-    public function listAction()
+    public function newArticleAction()
+    {
+        $user = $this->get('security.token_storage')->getToken()->getUser();
+        $userId = $user->getId();
+        $userName = $user->getUserName();
+        return $this->render(
+            'GarlicBlogAppBundle:Articles:newArticle.html.twig',
+            array(
+                // last username entered by the user
+                'userId'    => $userId,
+                'userName'  => $userName,
+            )
+        );
+    }
+    
+    /**
+     * @Route("/myArticles", name="my_articles")
+     */
+    public function myArticlesAction()
+    {
+        $user = $this->get('security.token_storage')->getToken()->getUser();
+        $userId = $user->getId();
+        $userName = $user->getUserName();
+        return $this->render(
+            'GarlicBlogAppBundle:Default:index.html.twig',
+            array(
+                // last username entered by the user
+                'userId'    => $userId,
+                'userName'  => $userName,
+            )
+        );
+    }
+    
+    /**
+     * @Route("/articles/{id}", name="single_article")
+     */
+    public function singleArticleAction()
     {
         return $this->render('GarlicBlogAppBundle:Articles:article.html.twig');
     }
