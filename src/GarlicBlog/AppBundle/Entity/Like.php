42d9b5e7de6a7ea03060dec6067818c1f9f92159
yaou-li <amor89813@gmail.com>
2016-08-10 01:06:53 -0400

    add new table for like and include the main js function

---
+++ src/GarlicBlog/AppBundle/Entity/Like.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace GarlicBlog\AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Like
+ *
+ * @ORM\Table(name="like", indexes={@ORM\Index(name="userid", columns={"userid"}), @ORM\Index(name="articleid", columns={"articleid"})})
+ * @ORM\Entity
+ */
+class Like
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var \GarlicBlog\AppBundle\Entity\Article
+     *
+     * @ORM\ManyToOne(targetEntity="GarlicBlog\AppBundle\Entity\Article")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="articleid", referencedColumnName="id")
+     * })
+     */
+    private $articleid;
+
+    /**
+     * @var \GarlicBlog\UserBundle\Entity\User
+     *
+     * @ORM\ManyToOne(targetEntity="GarlicBlog\UserBundle\Entity\User")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="userid", referencedColumnName="id")
+     * })
+     */
+    private $userid;
+
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set articleid
+     *
+     * @param \GarlicBlog\AppBundle\Entity\Article $articleid
+     *
+     * @return Favorite
+     */
+    public function setArticleid(\GarlicBlog\AppBundle\Entity\Article $articleid = null)
+    {
+        $this->articleid = $articleid;
+
+        return $this;
+    }
+
+    /**
+     * Get articleid
+     *
+     * @return \GarlicBlog\AppBundle\Entity\Article
+     */
+    public function getArticleid()
+    {
+        return $this->articleid;
+    }
+
+    /**
+     * Set userid
+     *
+     * @param \GarlicBlog\UserBundle\Entity\User $userid
+     *
+     * @return Favorite
+     */
+    public function setUserid(\GarlicBlog\UserBundle\Entity\User $userid = null)
+    {
+        $this->userid = $userid;
+
+        return $this;
+    }
+
+    /**
+     * Get userid
+     *
+     * @return \GarlicBlog\UserBundle\Entity\User
+     */
+    public function getUserid()
+    {
+        return $this->userid;
+    }
+}
